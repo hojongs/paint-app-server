5a00a3feae6dd9f1c38ca39220a429aed58d1b8d
Jongho Jeon <hojong.jjh@gmail.com>
2020-05-16 22:36:14 +0900

    Update PaintUser

--- src/main/kotlin/com/hojongs/paint/repository/model/PaintUser.kt
+++ src/main/kotlin/com/hojongs/paint/repository/model/PaintUser.kt
@@ -3,32 +3,33 @@ package com.hojongs.paint.repository.model
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.AuthorityUtils
 import org.springframework.security.core.userdetails.UserDetails
-import java.util.*
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @Entity(name = "paint_users")
 data class PaintUser(
     @Id
-    val id: UUID = UUID.randomUUID(),
-    private val nickname: String,
-    private val password: String
-) : UserDetails {
-
-    override fun getAuthorities(): Collection<GrantedAuthority> =
+    private val email: String,
+    private val displayName: String,
+    private val password: String,
+    private val authorities: List<GrantedAuthority> =
         AuthorityUtils.createAuthorityList(
             "ROLE_USER"
-        )
+        ),
+    private val isEnabled: Boolean = true
+) : UserDetails {
 
-    override fun isEnabled(): Boolean = true
-
-    override fun getUsername(): String = id.toString()
+    override fun getUsername(): String = email
 
     override fun getPassword(): String = password
 
-    override fun isCredentialsNonExpired(): Boolean = false
+    override fun getAuthorities(): Collection<GrantedAuthority> = authorities
 
-    override fun isAccountNonExpired(): Boolean = false
+    override fun isEnabled(): Boolean = isEnabled
 
-    override fun isAccountNonLocked(): Boolean = false
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
 }
