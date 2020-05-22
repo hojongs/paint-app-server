d4a8e74d7deac31284d031bf1b1aad192a2912cb
Jongho Jeon <hojong.jjh@gmail.com>
2020-05-23 03:42:00 +0900

    Add ComponentScan

--- src/main/kotlin/com/hojongs/paint/app/App.kt
+++ src/main/kotlin/com/hojongs/paint/app/App.kt
@@ -1,7 +1,17 @@
 package com.hojongs.paint.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication
+@ComponentScan(
+    "com.hojongs.paint.restcontroller",
+    "com.hojongs.paint.service"
+)
+@EnableWebSecurity
+@EnableJpaRepositories("com.hojongs.paint.repository")
+@EntityScan("com.hojongs.paint.repository.model")
 class App
