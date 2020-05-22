56f7cebb63b646dec17f3739e20a73b5be30e3c1
Jongho Jeon <hojong.jjh@gmail.com>
2020-05-23 03:42:34 +0900

    Delete Authorization

--- src/main/kotlin/com/hojongs/paint/configuration/WebAuthorizationConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.hojongs.paint.configuration
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
-
-@Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-class WebAuthorizationConfig
--- src/main/kotlin/com/hojongs/paint/service/HomeService.kt
+++ src/main/kotlin/com/hojongs/paint/service/HomeService.kt
@@ -2,7 +2,6 @@ package com.hojongs.paint.service
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,6 +9,5 @@ class HomeService {
 
     private val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
-    @PreAuthorize("hasRole('ADMIN')")
     fun getMsg(name: String) = "hello, $name"
 }
