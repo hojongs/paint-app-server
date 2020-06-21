22d5c9a12b5cc886f2f42ab1c0945f7b23732717
Jongho Jeon <hojong.jjh@gmail.com>
2020-06-21 10:18:18 +0900

    Remove annotation

--- src/main/kotlin/com/hojongs/paint/repository/PaintSessionRepository.kt
+++ src/main/kotlin/com/hojongs/paint/repository/PaintSessionRepository.kt
@@ -1,12 +1,10 @@
 package com.hojongs.paint.repository
 
 import com.hojongs.paint.model.PaintSession
-import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import reactor.core.publisher.Mono
 import java.util.UUID
 
-@Configuration
 interface PaintSessionRepository : ReactiveMongoRepository<PaintSession, UUID> {
     fun findByName(name: String): Mono<PaintSession>
     fun findByNameAndPassword(name: String, password: String): Mono<PaintSession>
--- src/main/kotlin/com/hojongs/paint/repository/PaintUserRepository.kt
+++ src/main/kotlin/com/hojongs/paint/repository/PaintUserRepository.kt
@@ -1,12 +1,10 @@
 package com.hojongs.paint.repository
 
 import com.hojongs.paint.model.PaintUser
-import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import reactor.core.publisher.Mono
 import java.util.UUID
 
-@Configuration
 interface PaintUserRepository : ReactiveMongoRepository<PaintUser, UUID> {
     fun findByEmail(email: String): Mono<PaintUser>
     fun findByEmailAndPassword(email: String, password: String): Mono<PaintUser>
