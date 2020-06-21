4a952f3955464b2a07e12e81db1def4b008c74a8
Jongho Jeon <hojong.jjh@gmail.com>
2020-06-21 11:10:23 +0900

    Impl notifyEvent

--- src/main/kotlin/com/hojongs/paint/service/PaintEventService.kt
+++ src/main/kotlin/com/hojongs/paint/service/PaintEventService.kt
@@ -2,23 +2,32 @@ package com.hojongs.paint.service
 
 import com.hojongs.paint.model.PaintEvent
 import com.hojongs.paint.repository.PaintEventRepository
+import com.hojongs.paint.repository.PaintUserRepository
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import java.util.UUID
 
 @Service
 class PaintEventService(
-    private val paintEventRepository: PaintEventRepository
+    private val paintEventRepository: PaintEventRepository,
+    private val paintUserRepository: PaintUserRepository
 ) {
     fun notifyEvent(
         userId: UUID,
         sessionId: UUID,
         eventType: PaintEvent.Type
     ): Mono<PaintEvent> {
-        val logEvent = Mono.empty<PaintEvent>()
-        val sendEvent = Mono.empty<PaintEvent>()
+        val paintEvent = PaintEvent(
+            userId = userId,
+            sessionId = sessionId,
+            eventType = eventType.name
+        )
+        val logEvent = paintEventRepository.save(paintEvent)
+        // todo send event to users
+        val sendEventToSessionUsers = paintUserRepository
+            .findByJoinedSessionId(sessionId)
 
         return logEvent
-            .flatMap { sendEvent }
+            .doOnNext { sendEventToSessionUsers.subscribe() }
     }
 }
