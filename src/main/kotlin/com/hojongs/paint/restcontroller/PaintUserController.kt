5e7a819be7d1c031f895c787278753a4d48a9025
Jongho Jeon <hojong.jjh@gmail.com>
2020-06-21 12:19:15 +0900

    Add /users/create

--- src/main/kotlin/com/hojongs/paint/restcontroller/PaintUserController.kt
+++ src/main/kotlin/com/hojongs/paint/restcontroller/PaintUserController.kt
@@ -1,8 +1,11 @@
 package com.hojongs.paint.restcontroller
 
+import com.hojongs.paint.model.PaintUser
 import com.hojongs.paint.service.PaintUserService
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Mono
+import java.util.UUID
 
 @RestController
 @RequestMapping("/users")
@@ -11,20 +14,17 @@ class PaintUserController(
 ) {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-//    @GetMapping("/create")
-//    fun createUser(
-//        @RequestParam email: String,
-//        @RequestParam password: String,
-//        @RequestParam("displayName") displayNameOrNull: String?
-//    ): Mono<PaintUser> {
-//        if (email.isEmpty())
-//            throw IllegalArgumentException("email=$email")
-//        if (password.isEmpty())
-//            throw IllegalArgumentException("password=$password")
-//
-//        val displayName = displayNameOrNull
-//            ?: email.also { logger.info("displayName was null, replaced with email($email)") }
-//
-//        return paintUserService.signUp(email, password, displayName)
-//    }
+    @GetMapping("/create")
+    fun createUser(
+        @RequestParam email: String,
+        @RequestParam password: String
+    ): Mono<UUID> {
+        if (email.isEmpty())
+            throw IllegalArgumentException("email=$email")
+        if (password.isEmpty())
+            throw IllegalArgumentException("pw=$password")
+
+        return paintUserService.createUser(email, password)
+            .map { it.id }
+    }
 }
