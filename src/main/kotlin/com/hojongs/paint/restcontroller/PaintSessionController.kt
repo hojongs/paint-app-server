76a37bd1044e2561ef8521dad6131f2351a48eb9
Jongho Jeon <hojong.jjh@gmail.com>
2020-06-21 12:38:17 +0900

    Add /sessions/create API

--- src/main/kotlin/com/hojongs/paint/restcontroller/PaintSessionController.kt
+++ src/main/kotlin/com/hojongs/paint/restcontroller/PaintSessionController.kt
@@ -1,32 +1,37 @@
 package com.hojongs.paint.restcontroller
 
+import com.hojongs.paint.model.PaintSession
+import com.hojongs.paint.service.AuthenticationService
+import com.hojongs.paint.service.PaintSessionService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Mono
+import java.util.UUID
 import kotlin.NoSuchElementException
 
 @RestController
 @RequestMapping("/sessions")
 class PaintSessionController(
-//    private val paintSessionService: PaintSessionService
+    private val paintSessionService: PaintSessionService,
+    private val authenticationService: AuthenticationService
 ) {
 
     private val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
-//    // todo user 체크 & session에 대한 권한 체크
-//    @GetMapping("/{id}")
-//    private fun findById(@PathVariable id: String): PaintSession {
-//        return paintSessionService.findById(id)
-//    }
-//
-//    // todo user 체크
-//    @GetMapping("/create")
-//    private fun createOne(@RequestParam name: String, @RequestParam password: String): PaintSession {
-//        return paintSessionService.createPaintSession(name, password)
-//    }
-//
+    @GetMapping("/create")
+    private fun createSession(
+        @RequestParam userId: UUID,
+        @RequestParam name: String,
+        @RequestParam password: String
+    ): Mono<PaintSession> {
+        return authenticationService
+            .authenticate(userId)
+            .flatMap { paintSessionService.createSession(userId, name, password) }
+    }
+
 //    // todo user 체크
 //    @GetMapping
 //    private fun listPage(@RequestParam(defaultValue = "0") pageNumber: Int): List<PaintSession> =
